60474fae7068f8162be0d68848858631c4f224f7
Juan Moreno <morenostudent1@gmail.com>
2021-12-27 12:40:13 -0800

    Update Header.jsx

--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         </header>
         <div className="filter-buttons">
         <a href="index.html"><button className="filter-btn c-btn btn-primary">Home</button></a>
-        <a href="people.html"><button className="filter-btn">Funny</button></a>
+        <a href="people.html"><button className="filter-btn">LOL</button></a>
         <a href="things.html"><button className="filter-btn">Random</button></a>
                 <button className="filter-btn login-btn btn-primary">Log In <FaChevronRight /> </button>
             </div>
